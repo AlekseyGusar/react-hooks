9f7e15fadf1b9f3c00f1cccdcbd0cde4bb05b09b
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-05-17 20:49:56 +0300

    add hook useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  return <div> React Marathon </div>;
+  const [text, setText] = useState('React Marathon');
+  
+  return <div onClick={() => setText(text.toLowerCase())}>{ text }</div>;
 }
 
